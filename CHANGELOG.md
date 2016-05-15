1e33f0e12201c5456e9ffb06fd3566e1b0ca606c
Pedro Ferreira <pedro@phower.com>
2016-05-15 14:42:19 +0100

    Release 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 CHANGELOG
 =========
 
+1.0.0
+-----
+
+ * Initial release
--- src/Stack.php
+++ src/Stack.php
@@ -3,7 +3,7 @@
 /**
  * Phower Arrays
  *
- * @version 0.0.0
+ * @version 1.0.0
  * @link https://github.com/phower/arrays Public Git repository
  * @copyright (c) 2015-2016, Pedro Ferreira <https://phower.com>
  * @license https://opensource.org/licenses/MIT MIT
